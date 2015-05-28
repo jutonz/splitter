bc1e7958e695940751eb22e6d73ec9f9e53b854d
Justin Toniazzo <jutonz42@gmail.com>
2015-05-28 02:05:19 -0400

    Improved error handling

--- lib/splitter.rb
+++ lib/splitter.rb
@@ -1,17 +1,24 @@
 require 'splitter/version.rb'
 
-# Add requires for other files you add to your project here, so
-# you just need to require this one file in your bin file
-
 module Splitter 
   class Splitter
     attr_accessor :file_to_split, :cuefile
 
     def initialize(file_to_split, cuefile)
-      raise "The file #{file_to_split} doesn't exist" unless File.exist? file_to_split
+      fail_unless_exist file_to_split, cuefile
 
       self.file_to_split = file_to_split
       self.cuefile = cuefile
+    end
+
+    private
+
+    def fail_unless_exist(*files)
+      files.each { |file| fail_unless_exists file }
+    end
+
+    def fail_unless_exists(file)
+      raise "The file #{file} doesn't exist" unless File.exist? file
     end
 
   end
